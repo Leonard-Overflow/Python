7ad2cad85e719822e6a8f96848e06f6c38c167b6
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-06-02 14:31:42 -0300

    Arrumando algumas opcoes e testando opcoes 1, 2, 3, 5 e 6

--- basico/modulos/built_in/collections/chain_map.py
+++ basico/modulos/built_in/collections/chain_map.py
@@ -11,7 +11,8 @@ Global = {
     "nome" : "calculadora",
     "tipo" : "python",
     "bibliotecas_instaladas" : ["numpy", "pandas", "keras", "tensorflow"],
-    "variaeis_funcoes" : [[], []]
+    "variaveis_funcoes" : [[], []],
+    "decorators" : []
 }
 
 Builtin = {
@@ -33,7 +34,7 @@ while True:
                          "6 - Criar uma variavel\n"
                          "7 - Executar funcao\n"
                          "8 - Atualizar configuracoes\n"
-                         "9 - Sair"))
+                         "9 - Sair\n"))
 
     match resposta:
         case 1:
@@ -59,13 +60,13 @@ while True:
                 print(Local["resultado"])
         case 2:
             resposta = input("Voce deseja ver as configuracoes? do global ou do bulit-in?\n")
-            if resposta == "global" and len(escopos) == 3:
+            if resposta == "global" and len(escopos.maps) == 3:
                 print(escopos.maps[1])
-            elif resposta == "global" and len(escopos) > 3:
+            elif resposta == "global" and len(escopos.maps) > 3:
                 print(escopos.maps[2])
-            elif resposta == "built-in" and len(escopos) == 3:
+            elif resposta == "built-in" and len(escopos.maps) == 3:
                 print(escopos.maps[2])
-            elif resposta == "built-in" and len(escopos) > 3:
+            elif resposta == "built-in" and len(escopos.maps) > 3:
                 print(escopos.maps[3])
         case 3:
             resposta = input("Onde deseja instalar a biblioteca, no projeto ou no computador?\n")
@@ -78,18 +79,21 @@ while True:
                     Global["bibliotecas_instaladas"].append(lib)
                     sleep(3)
                     print("Biblioteca instalada.")
-            else:
+            elif resposta == "computador":
                 lib = input("Digite o comando de instalacao\n")
-                if not lib.startswith("pip install "):
+                if lib.startswith("pip install "):
+                    lib = lib[12::].strip().lower()
                     if lib in Builtin["bibliotecas_instaladas"]:
                         print(f"A biblioteca {lib} ja esta instalada.")
                     else:
                         print("instalando...")
                         Builtin["bibliotecas_instaladas"].append(lib)
-                        sleep(3)
+                        sleep(1)
                         print("Biblioteca instalada.")
                 else:
                     print("Erro. Use o comando pip install para instalar")
+            else:
+                print("Insira uma resposta valida!")
         case 4:
             pass
         case 5:
@@ -99,15 +103,32 @@ while True:
                 parametro = input("Insira o nome do parametro(enter para parar): ").strip().lower()
                 if parametro == "":
                     break
-                parametros.append(parametro)
+            parametros.append(parametro)
             retorno = input("Qual o retorno da funcao?\n").lower().strip()
             parametros = ", ".join(parametros)
             lambda_str = f"lambda {parametros}: {retorno}"
             funcao = eval(lambda_str)
-            Global["variaveis_funcoes"].append(funcao)
+            Global["variaveis_funcoes"][1].append(funcao)
         case 6:
             nome = input("Qual o nome da variavel?\n").lower().strip()
-            valor = float(input("Digite o valor da variavel: "))
+            tipo = input("Qual o tipo da variavel?\n").lower().strip()
+
+            match tipo:
+                case "string":
+                    valor = input("Insira o valor da variavel?\n").lower().strip()
+                case "int":
+                    try:
+                        valor = int(input("Insira o valor da variavel?\n"))
+                    except ValueError:
+                        raise("Insira um valor valido!")
+                case "float":
+                    try:
+                        valor = float(input("Insira o valor da variavel?\n"))
+                    except ValueError:
+                        raise("Insira um valor valido!")
+                case _:
+                    raise ValueError("Insira um tipo valido!")
+
             tupla_da_variavel = (nome, valor)
             Global["variaveis_funcoes"][0].append(tupla_da_variavel)
         case 7:
