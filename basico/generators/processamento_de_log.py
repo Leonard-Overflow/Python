f57bf2cad5d611c774a2a103fa5e73e26b3b3793
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-06-10 19:19:50 -0300

    Generators prontos

--- basico/generators/processamento_de_log.py
+++ basico/generators/processamento_de_log.py
@@ -1,15 +1,30 @@
-def ler_arquivo(arquivo):
-    with open(arquivo) as arquivo:
+def ler_logs(arquivo):
+    with open(arquivo, "r") as arquivo:
         for linha in arquivo:
-            if "INFO" in linha:
-                continue
-            partes = linha.split(" ", 2)
+            partes = tuple(linha.split(' ', 2))
+            yield partes
 
-            yield {
-                "data": partes[0],
-                "tipo" : partes[1],
-                "mensagem" : partes[2]
-            }
+def filtro_de_nivel(gen, nivel):
+    for retorno in gen:
+        if retorno[1] == nivel:
+            yield retorno
+        else:
+            continue
 
-def contador_de_log(arquivo):
-    log_limpo = ler_arquivo(arquivo)
+def duplicatas(gen):
+    dict_duplicatas = {}
+
+    for retorno in gen:
+        if retorno[2] not in dict_duplicatas:
+            dict_duplicatas[retorno[2]] = 1
+        else:
+            dict_duplicatas[retorno[2]] += 1
+
+    return dict_duplicatas
+
+leitura = ler_logs("logs_exercicio.txt")
+filtro = filtro_de_nivel(leitura, "ERROR")
+duplicatas = duplicatas(filtro)
+
+for item in duplicatas.items():
+    print(item)
