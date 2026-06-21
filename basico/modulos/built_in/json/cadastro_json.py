d7c415f80ccaee53a5e9773d0d4ec90e3045454a
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-06-21 18:29:44 -0300

    Projeto de json pronto

--- basico/modulos/built_in/json/cadastro_json.py
+++ basico/modulos/built_in/json/cadastro_json.py
@@ -11,32 +11,72 @@ while True:
     match escolha:
         case "1":
             with open("users.json", "r", encoding="utf-8") as file:
-                user = json.load(file)
+                users = json.load(file)
 
             user_dict = {}
             user_dict["nome"] = input("Qual o nome do usuario: ")
             user_dict["senha"] = input("Qual a senha do usuario: ")
-            user.append(user_dict)
+            users.append(user_dict)
             del user_dict
 
             with open("users.json", "w", encoding="utf-8") as file:
-                json.dump(user, file, indent=4, ensure_ascii=False)
+                json.dump(users, file, indent=4, ensure_ascii=False)
 
-            del user # Otimizar a memoria RAM
+            del users # Otimizar a memoria RAM
 
         case "2":
             with open("users.json", "r", encoding="utf-8") as file:
-                user = json.load(file)
+                users = json.load(file)
 
             print(f"{"USUARIO": ^30}\n")
-            for i, dicionario in enumerate(user, start=1):
+            for i, dicionario in enumerate(users, start=1):
                 print(f"{i}. {dicionario["nome"]: >30}")
 
-            del user
+            del users
         case "3":
-            pass # Atualizar os dados do JSON se não existir lançar aviso
+            with open("users.json", "r", encoding="utf-8") as file:
+                users = json.load(file)
+
+            while True:
+                for i, dicionario in enumerate(users, start=1):
+                    print(f"{i}. {dicionario["nome"]: >30}")
+                usuario = input("Qual o nome do usuario que sera alterado: ")
+                for item in users:
+                    if item["nome"] == usuario:
+                        item["nome"] = input("Qual o novo nome do usuario: ")
+                        item["senha"] = input("Qual a senha do usuario: ")
+                        break
+                    else:
+                        continue
+                resposta = input("O usuario nao existe. Deseja sair?(S)")
+                if resposta == "S":
+                    break
+                else:
+                    continue
+            with open("users.json", "w", encoding="utf-8") as file:
+                json.dump(users, file, indent=4, ensure_ascii=False)
+            del users
         case "4":
-            pass # Apagar um dado do JSON
+            with open("users.json", "r", encoding="utf-8") as file:
+                users = json.load(file)
+            while True:
+                for i, dicionario in enumerate(users, start=1):
+                    print(f"{i}. {dicionario["nome"]: >30}")
+                usuario = input("Qual o nome do usuario que sera removido: ")
+                for item in users:
+                    if item["nome"] == usuario:
+                        users.remove(item)
+                        break
+                    else:
+                        continue
+                resposta = input("O usuario nao existe. Deseja sair?(S)")
+                if resposta == "S":
+                    break
+                else:
+                    continue
+            with open("users.json", "w", encoding="utf-8") as file:
+                json.dump(users, file, indent=4, ensure_ascii=False)
+            del users
         case "5":
             break
         case _:
