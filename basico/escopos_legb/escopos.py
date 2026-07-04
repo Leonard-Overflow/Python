bfa49405a1d0207c6ac1232e3bcf73b1efd9fb43
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-07-04 20:00:20 -0300

    Finalmente entendendo escopos

---
+++ basico/escopos_legb/escopos.py
@@ -0,0 +1,15 @@
+import numpy as np # Dentro do global
+
+def funcao1(): # Dentro do global
+    print("funcao 1") # Dentro do local de funcao1(quando executada)
+
+def funcao2(): # Dentro do global. Serve de enclousing para funcao3
+    valor = 10 # Dentro do local de funcao2(quando executada)
+    def funcao3(mensagem): # Dentro do local de funcao2 e a closure que guarda a cell de memória(que guarda a variavel valor)
+        print(valor + mensagem) # Escopo local de funcao3(quando executada)
+    return funcao3 # Dentro de local da funcao2
+
+# Todos dentro de global
+funcao1()
+variavel = funcao2()
+variavel(5)
