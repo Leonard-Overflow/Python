b792c28c6bb878d541ab7b560673473702e9e462
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-07-06 11:22:23 -0300

    Continuacao de comprehension

--- basico/comprehensions/list_comprehension.py
+++ basico/comprehensions/list_comprehension.py
@@ -1,14 +1,24 @@
 mochila = [
-    {"nome": "pocao de Cura","tipo": "pocao","poder": 80},
-    {"nome": "espada de Madeira", "tipo": "arma", "poder": 15},
-    {"nome": "pergaminho de Fogo", "tipo": "magia", "poder": 95},
-    {"nome": "pocao de Mana", "tipo": "pocao", "poder": 40},
-    {"nome": "escudo de Cobre", "tipo": "defesa", "poder": 25},
-    {"nome": "amuleto do Dragão", "tipo": "acessório", "poder": 100},
-    {"nome": "pocao de Invisibilidade", "tipo": "pocao", "poder": 60}
+    {"nome": "pocao de cura","tipo": "pocao","poder": 80},
+    {"nome": "espada de madeira", "tipo": "arma", "poder": 15},
+    {"nome": "pergaminho de fogo", "tipo": "magia", "poder": 95},
+    {"nome": "pocao de mana", "tipo": "pocao", "poder": 40},
+    {"nome": "escudo de cobre", "tipo": "defesa", "poder": 25},
+    {"nome": "amuleto do dragão", "tipo": "acessório", "poder": 100},
+    {"nome": "pocao de invisibilidade", "tipo": "pocao", "poder": 60}
 ]
 
 # Nivel 1
 iventario = [item["nome"] for item in mochila]
-pocoes = [item["tipo"] for item in mochila if item["tipo"] == "pocao"]
-poderosos = [item["poder"] for item in mochila if item["poder"] >= 50]
+pocoes = [item["nome"] for item in mochila if item["tipo"] == "pocao"]
+poderosos = [item["nome"] for item in mochila if item["poder"] >= 50]
+
+# Nivel 2
+poderosos2 = [item["nome"].upper() if item["poder"] >= 50 else item["nome"].lower() for item in mochila]
+magias_pocoes = [item["nome"] for item in mochila if item["poder"] > 50 and item["tipo"] == "magia" or item["tipo"] == "pocao"]
+
+# Nivel 3
+nome_poder = {item["nome"] : item["poder"] for item in mochila}
+tipos_itens = {item["tipo"] for item in mochila}
+mais_poderosos = {item["nome"] : item["poder"] for item in mochila}
+mais_poderosos = sorted(mais_poderosos, key=mais_poderosos.get)
