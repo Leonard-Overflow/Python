688d1a7f1d11ae780563485f47cf7dabbf45e591
Leonardo Rafael de Castro <lrccastro4@gmail.com>
2026-07-14 20:48:28 -0300

    Inicio da recuperacao

--- basico/funcoes_de_ordem_superior/map.py
+++ basico/funcoes_de_ordem_superior/map.py
@@ -8,6 +8,18 @@ folha_de_pagamento = {
     "Leonardo" : 3500
 }
 
-salarios_com_aumento = [x for x in list(map(lambda a: a + 200, folha_de_pagamento.values()))]
+# Com funcao anonima
 
+salarios_com_aumento = [x for x in list(map(lambda a: a + 200, folha_de_pagamento.values()))]
 print(salarios_com_aumento)
+
+# Com funcao pre-escrita
+def somar_listas(lista1, lista2):
+    return lista1 + lista2
+
+lista_1 = [1, 2, 3, 4, 5]
+lista_2 = [6, 7, 8, 9, 10]
+soma = list(map(somar_listas, lista_1, lista_2)) # duas listas -> funcao recebe 2 parametros
+print(soma)
+
+# map() retorna um obj map e calcula os valores conforme itera sobre o iteravel. Processamento lazy, so demanda
